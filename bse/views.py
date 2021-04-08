048e6d5da1dcb4655cb108ca2f4bc479e97fdcd7
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-04-08 12:49:36 +0530

    remove comment

--- bse/views.py
+++ bse/views.py
@@ -104,7 +104,6 @@ def bhav_bse(request):
   """
   template_name = 'bhav_bse.html'
   # Connect to redis
-  # redis://:p5b1ac14b89b477151716d1ecea3bb6e645a128d3b0ab310021b1b1e28290ac4f@ec2-52-4-141-196.compute-1.amazonaws.com:24399
   redis_url = os.environ.get('REDIS_URL')
   if redis_url:
     redis_instance = redis.from_url(redis_url)
