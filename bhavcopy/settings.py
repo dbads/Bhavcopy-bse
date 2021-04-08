e78e07e293bce785da643869d31e6188fbf81c5d
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-04-08 12:08:10 +0530

    change static file storage

--- bhavcopy/settings.py
+++ bhavcopy/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
