1b88f33744bf6c6573fb848e8e742d1a97435bbe
Deepak Bharti <deepakbharti@mnnit.ac.in>
2021-04-10 11:50:40 +0530

    hide secret key

--- bhavcopy/settings.py
+++ bhavcopy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y(1tgy-^q#rka5&82i7&q%9q293mj(=tch0gg_qf0zpin60hwf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
